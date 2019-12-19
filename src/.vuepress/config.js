d2019ae3c711782238596c22d8bbe2697a5fae14
lianyuansheng <lianyuansheng@xiao100.com>
2019-12-20 00:41:42 +0800

    fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,5 +3,6 @@ const path = require("path")
 module.exports = {
     title: 'laoergege blog',
     description: 'Just For Fun',
-    dest: path.resolve(__dirname, '../../docs')
+    dest: path.resolve(__dirname, '../../docs'),
+    base: '/laoregege-blog/'
   }
