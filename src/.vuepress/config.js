f145dcd150596a5de867260177f5c651769b21a3
lianyuansheng <lianyuansheng@xiao100.com>
2019-12-24 00:16:35 +0800

    fix

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,5 +3,14 @@ const path = require("path")
 module.exports = {
     title: 'laoergege blog',
     description: 'Just For Fun',
-    dest: path.resolve(__dirname, '../../docs')
+    dest: path.resolve(__dirname, '../../docs'),
+
+    // theme
+    themeConfig: {
+      sidebarDepth: 2,
+      displayAllHeaders: true,
+      sidebar: [
+        '/工具使用篇/Git/Git 使用'
+      ]
+    }
   }
