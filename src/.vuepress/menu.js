42e8c7cf248d5561cd9e21ab3f7af450f3f63ded
lianyuansheng <lianyuansheng@xiao100.com>
2020-03-29 23:14:57 +0800

    修复侧边栏菜单名 BUG

--- src/.vuepress/menu.js
+++ src/.vuepress/menu.js
@@ -21,7 +21,7 @@ module.exports = {
             {
                 title: 'Git 博客开发系列',
                 children: [
-                    ['使用 GithubPage、GithubAction、VuePress 搭建个人博客']
+                    ['/使用 GithubPage、GithubAction、VuePress 搭建个人博客']
                 ]
             },
             'GithubAction'
