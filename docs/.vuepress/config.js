1336d772243c5c409c32e4c71dfc724499a3f846
连远生 <lianyuansheng@xiao100.com>
2021-05-18 20:34:38 +0800

    js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,11 @@ const path = require("path")
 const menu = require("./menu")
 const markdown = require("./markdown")
 const configWebpack = require('./webpack-chain')
-// const plugins = require('./plugins')
+const plugins = require('./plugins')
 
 module.exports = {
   // 基本配置
-  title: "Laoergege Blog",
+  title: "",
   description: "Just For Fun",
   dest: path.resolve(__dirname, "../../dist"),
   extraWatchFiles: ["./menu.js"],
@@ -23,7 +23,7 @@ module.exports = {
   markdown,
 
   // plugins
-  // plugins,
+  plugins,
 
   chainWebpack: configWebpack,
 };
