329ea13bafcac4b73b13383b08c470714c6f8214
连远生 <lianyuansheng@xiao100.com>
2023-07-08 15:29:47 +0800

    feat(css)：添加全局 select 样式

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
   <NuxtPage />
 </template>
+
+<style lang="postcss">
+#__nuxt ::selection {
+  color: theme("textColor.accent-content");
+  background-color: theme("textColor.accent");
+}
+</style>
