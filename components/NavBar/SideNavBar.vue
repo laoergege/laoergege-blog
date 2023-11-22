02e3a7f6abdc5a1afee8878af9c25e06c570ab36
连远生 <lianyuansheng@xiao100.com>
2023-11-22 15:37:25 +0800

    fix

--- components/NavBar/SideNavBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="h-screen sticky top-0 none">
-    <Menu />
-  </div>
-</template>
-
-<script setup>
-import Menu from "~/components/NavBar/Menu.vue";
-</script>
--- server/plugins/images.ts
+++ server/plugins/images.ts
@@ -2,6 +2,7 @@ import { resolve, basename } from "node:path";
 import { mkdir } from "node:fs/promises";
 import { symlinkSync, existsSync } from "node:fs";
 import { URL, pathToFileURL, fileURLToPath } from "node:url";
+import { visit } from 'unist-util-visit'
 
 export default defineNitroPlugin((nitroApp) => {
   const destDir = resolve("public", "images")
@@ -10,11 +11,11 @@ export default defineNitroPlugin((nitroApp) => {
   // @ts-ignore
   nitroApp.hooks.hook("content:file:beforeParse", (file: ContentFile) => {
     if (file._id.endsWith(".md")) {
-      file.body = file.body.replace(
+      file.body = file.body.replaceAll(
         /(?<=\!\[.*?\]\()\.\/.*?\.(png|gif|jpg|jpeg|svg)(?=\))/g,
         (img: string) => {
           let { _id } = file;
-          const resolveImgPath = fileURLToPath(new URL(img, pathToFileURL(encodeURI(resolve(..._id.split(":"))))))
+          const resolveImgPath = fileURLToPath(new URL(img, pathToFileURL(resolve(..._id.split(":")))))
           const imgName = basename(resolveImgPath)
           const destImg = resolve(destDir, imgName)
 
@@ -27,4 +28,22 @@ export default defineNitroPlugin((nitroApp) => {
       );
     }
   });
+
+  // @ts-ignore
+  nitroApp.hooks.hook('content:file:afterParse', async (file) => {
+    if (file._id.endsWith('.md')) {
+      // @ts-ignore
+      const imgNodes = []
+      visit(file.body, (n: any) => n.tag === 'img', (node) => {
+        imgNodes.push(node)
+      })
+
+      // @ts-ignore
+      for await (const node of imgNodes) {
+        const { width, height } = await fetchImgSize(node.props.src);
+        node.props.width = width
+        node.props.height = height
+      }
+    }
+  })
 });
