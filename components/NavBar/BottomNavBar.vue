4b32aa329c7465314b02a4038ae646a6a9088861
连远生 <lianyuansheng@xiao100.com>
2023-11-22 22:29:08 +0800

    fix

--- components/NavBar/BottomNavBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="sticky bottom-0 md:hidden">
-    <Menu class="menu-horizontal w-full" />
-  </div>
-</template>
-
-<script setup>
-import Menu from "~/components/NavBar/Menu.vue";
-</script>
